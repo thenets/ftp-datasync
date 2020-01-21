5dc1d9c119fca6c153bb3617132d45a2436aefd5
Luiz Costa <luiz@thenets.org>
2020-01-21 04:20:41 -0300

    Better main function for testing

--- app.go
+++ app.go
@@ -2,25 +2,45 @@ package main
 
 import (
 	"fmt"
+	"os"
 
 	ftp_op "github.com/thenets/ftp-datasync/ftp-op"
 )
 
 func main() {
-	fmt.Println("Loading...")
-	sample()
-}
 
-func sample() {
-	// Load config and connect
-	context := ftp_op.ServerContext{
-		ConfigFilePath: "./test/server.yml",
+	if len(os.Args) != 3 {
+		fmt.Println("[ERROR] arguments not supplied!")
+		fmt.Println("How to use:")
+		fmt.Println("./ftpdatasync <configFilePath> <reportDestinationFilePath>")
+	
+		return
 	}
+
+	// Load args
+	configFilePath := os.Args[1]
+	reportDestinationFilePath := os.Args[2]
+
+	// Load config and create context
+	fmt.Printf("# Load config file...\n")
+	context := ftp_op.ServerContext{
+		ConfigFilePath: configFilePath,
+	}
+
+	// Connect
+	fmt.Printf("# Connect to remote server...\n")
 	context.Connect()
 	defer context.Disconnect()
 
+	// Sync remote and local dir
+	fmt.Printf("# Sync remote and local dir...\n")
 	context.Sync()
-	context.Compress()
-	context.CompressCreateReport("./test/report.csv")
-}
 
+	// Compress
+	fmt.Printf("\n# Compress...\n")
+	context.Compress()
+
+	// Create report
+	fmt.Printf("\n# Generate compress report...\n")
+	context.CompressCreateReport(reportDestinationFilePath)
+}
